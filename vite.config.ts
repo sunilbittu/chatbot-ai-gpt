98c5e503d9e24a2e23e80b0abc9476c264b9bde7
sunilbittu <pavanksunil@gmail.com>
2025-05-18 00:25:36 +0530

    Migrate from Tailwind to SCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {}
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/abstracts/variables"; @import "./src/styles/abstracts/mixins";`
+      }
+    }
   }
 });
